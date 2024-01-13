96a08eb210106c80b432c68eb71c388ee3300e21
roemer2201 <roemer2201@users.noreply.github.com>
2024-01-13 20:42:01 +0100

    Update ddpar-check.sh: Make the script actually work

--- ddpar-check.sh
+++ ddpar-check.sh
@@ -18,6 +18,7 @@ function show_help {
   echo "Optionen:"
   echo "-p, --path PATH         Speicherort des Splitimages"
   echo "-n, --name NAME         Der Basisname des geteilten Abbildes"
+  echo "-d                      Destination to compare against"
   echo "-h, --help              Zeigt diese Hilfemeldung an"
 }
 
@@ -34,13 +35,14 @@ while getopts ":p:n:h" opt; do
   case $opt in
     p|-path) INPUT_PATH="$OPTARG";;
     n|-name) INPUT_FILE_BASENAME="$OPTARG";;
+    d) DESTINATION="$OPTARG" ;;
     h|-help) show_help; exit 0;;
     \?) echo "Ungültige Option: -$OPTARG";;
   esac
 done
 
 # Überprüfung der erforderlichen Parameter
-if [ -z "$INPUT_PATH" ] || [ -z "$INPUT_FILE_BASENAME" ]; then
+if [ -z "${INPUT_PATH}" ] || [ -z "${INPUT_FILE_BASENAME}" ]; then
   echo "Fehlende Parameter. Bitte geben Sie alle erforderlichen Parameter an."
   exit 1
 fi
@@ -48,10 +50,12 @@ fi
 # Create spinoff variables
 INPUT_FILES="${INPUT_PATH}/${INPUT_FILE_BASENAME}-"
 METADATA_FILE="${INPUT_FILES}metadata.txt"
+OUTPUT_FILE=$DESTINATION
+OUTPUT_FILE_TYPE="$(file -b $DESTINATION)"
 
-echo $INPUT_PATH
-echo $INPUT_FILE_BASENAME
-echo $INPUT_FILES
+echo ${INPUT_PATH}
+echo ${INPUT_FILE_BASENAME}
+echo ${INPUT_FILES}*
 
 # Get parameters from metadata file
 NUM_JOBS=$(grep "NUM_JOBS" $METADATA_FILE | cut -d "=" -f 2)
@@ -59,13 +63,13 @@ SPLIT_SIZE=$(grep "SPLIT_SIZE" $METADATA_FILE | cut -d "=" -f 2)
 INPUT_FILE_TYPE=$(grep "FILE_TYPE" $METADATA_FILE | cut -d "=" -f 2)
 BLOCKSIZEBYTES=$(grep "BLOCKSIZEBYTES" $METADATA_FILE | cut -d "=" -f 2)
 
-echo $INPUT_FILE_TYPE
-echo $OUTPUT_FILE_TYPE
-if [[ "$INPUT_FILE_TYPE" == "block special"* ]] && [[ "$OUTPUT_FILE_TYPE" == "block special"* ]]; then
+echo ${INPUT_FILE_TYPE}
+echo ${OUTPUT_FILE_TYPE}
+if [[ "${INPUT_FILE_TYPE}" == "block special"* ]] && [[ "${OUTPUT_FILE_TYPE}" == "block special"* ]]; then
   echo "Beginning to check ..."
   check_restored_image
 fi
-if [[ "$INPUT_FILE_TYPE" != "block special"* ]] && [[ "$OUTPUT_FILE_TYPE" != "block special"* ]]; then
+if [[ "${INPUT_FILE_TYPE}" != "block special"* ]] && [[ "${OUTPUT_FILE_TYPE}" != "block special"* ]]; then
   echo "Beginning to check ..."
   check_restored_image
 fi
