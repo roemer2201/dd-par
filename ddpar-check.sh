2d7ad085f967489030abf9d3bdffd5fa2e3f5c62
roemer2201 <5441568+roemer2201@users.noreply.github.com>
2024-01-18 23:32:52 +0100

    Updated ddpar-check.sh: Skript now checks for SOURCE, BACKUP and DESTINATION and could compare two of them. Actual comparison commands need to be implemented.

--- ddpar-check.sh
+++ ddpar-check.sh
@@ -7,8 +7,8 @@
 #OUTPUT_FILE_BASENAME=sdi
 #OUTPUT_FILE="${OUTPUT_PATH}/${OUTPUT_FILE_BASENAME}"
 #OUTPUT_FILE_TYPE="$(file -b $OUTPUT_FILE)"
-INPUT_PATH=""
-INPUT_FILE_BASENAME=""
+BASE_PATH=""
+BASE_FILE_NAME=""
 
 function show_help {
   SCRIPT_NAME=$(basename "$0")
@@ -25,8 +25,16 @@ function show_help {
 function check_restored_image {
   for ((i=0; i<$NUM_JOBS; i++)); do
     START=$((i * SPLIT_SIZE))
-    echo "dd if=$OUTPUT_FILE bs=$BLOCKSIZEBYTES count=$((SPLIT_SIZE / $BLOCKSIZEBYTES)) skip=$((START / BLOCKSIZEBYTES)) status=none | sha256sum -c $INPUT_FILES$i.sha256 | sed s#-#$INPUT_FILES$i# &"
-    dd if=$OUTPUT_FILE bs=$BLOCKSIZEBYTES count=$((SPLIT_SIZE / $BLOCKSIZEBYTES)) skip=$((START / BLOCKSIZEBYTES)) status=none | sha256sum -c $INPUT_FILES$i.sha256 | sed s#-#$INPUT_FILES$i# &
+    echo "dd if=$OUTPUT_FILE bs=$BLOCKSIZEBYTES count=$((SPLIT_SIZE / $BLOCKSIZEBYTES)) skip=$((START / BLOCKSIZEBYTES)) status=none | sha256sum -c $BASE_FILES$i.sha256 | sed s#-#$BASE_FILES$i# &"
+    dd if=$OUTPUT_FILE bs=$BLOCKSIZEBYTES count=$((SPLIT_SIZE / $BLOCKSIZEBYTES)) skip=$((START / BLOCKSIZEBYTES)) status=none | sha256sum -c $BASE_FILES$i.sha256 | sed s#-#$BASE_FILES$i# &
+  done
+}
+
+function check_backuped_image {
+  for ((i=0; i<$NUM_JOBS; i++)); do
+    START=$((i * SPLIT_SIZE))
+    echo "dd if=$OUTPUT_FILE bs=$BLOCKSIZEBYTES count=$((SPLIT_SIZE / $BLOCKSIZEBYTES)) skip=$((START / BLOCKSIZEBYTES)) status=none | sha256sum -c $BASE_FILES$i.sha256 | sed s#-#$BASE_FILES$i# &"
+    dd if=$OUTPUT_FILE bs=$BLOCKSIZEBYTES count=$((SPLIT_SIZE / $BLOCKSIZEBYTES)) skip=$((START / BLOCKSIZEBYTES)) status=none | sha256sum -c $BASE_FILES$i.sha256 | sed s#-#$BASE_FILES$i# &
   done
 }
 
@@ -34,11 +42,11 @@ function check_restored_image {
 while getopts ":b:s:d:h" opt; do
   case $opt in
     b)
-        INPUT_PATH=$(dirname $(realpath "$OPTARG")); echo "Set INPUT_PATH=${INPUT_PATH}"
-	INPUT_FILE_BASENAME=$(basename $(realpath "$OPTARG")); echo "Set INPUT_FILE_BASENAME=${INPUT_FILE_BASENAME}"
-        ;;
-       
-#    n) echo "Set INPUT_FILE_BASENAME=$OPTARG"; INPUT_FILE_BASENAME="$OPTARG";; # Not needed anymore due to combined b ( = p + n )
+      BASE_PATH=$(dirname $(realpath "$OPTARG")); echo "Set BASE_PATH=${BASE_PATH}"
+      BASE_FILE_NAME=$(basename $(realpath "$OPTARG")); echo "Set BASE_FILE_NAME=${BASE_FILE_NAME}"
+      ;;
+#    n) echo "Set BASE_FILE_NAME=$OPTARG"; BASE_FILE_NAME="$OPTARG";; # Not needed anymore due to combined b ( = p + n )
+    s) echo "Set SOURCE=$OPTARG"; SOURCE="$OPTARG" ;;
     d) echo "Set DESTINATION=$OPTARG"; DESTINATION="$OPTARG" ;;
     h|-help) show_help; exit 0;;
     \?) echo "Ungültige Option: -$OPTARG";;
@@ -49,37 +57,90 @@ done
 # Extend the check to be able to compare one of source <-> backup, backup <-> destination, source <-> destination,
 # by making sure, that only 2 out of those 3 parameters are set.
 # Currently only backup <-> destination works.
-if [ -z "${INPUT_PATH}" ] || [ -z "${INPUT_FILE_BASENAME}" ]; then
-  echo "Fehlende Parameter. Bitte geben Sie alle erforderlichen Parameter an."
+
+# Check if only one of the three variables is set
+if { [ -n "$SOURCE" ] && [ -z "$BASE_PATH" ] && [ -z "$DESTINATION" ]; } \
+|| { [ -z "$SOURCE" ] && [ -n "$BASE_PATH" ] && [ -z "$DESTINATION" ]; } \
+|| { [ -z "$SOURCE" ] && [ -z "$BASE_PATH" ] && [ -n "$DESTINATION" ]; }; then
+  echo "Only one of the three variables is set."
   exit 1
 fi
 
+# Check if none of the three variables is set
+if [ -z "$SOURCE" ] && [ -z "$BASE_PATH" ] && [ -z "$DESTINATION" ]; then
+  echo "None of the three variables is set."
+  exit 1
+fi
+
+# Check if all three variables are set
+if [ -n "$SOURCE" ] && [ -n "$BASE_PATH" ] && [ -n "$DESTINATION" ]; then
+  echo "All three variables are set. The loop will not be executed."
+fi
+
 # Create spinoff variables
-INPUT_FILES="${INPUT_PATH}/${INPUT_FILE_BASENAME}-"
-METADATA_FILE="${INPUT_FILES}metadata.txt"
+if [ ! -z "${BASE_PATH}" ]; then 
+  BASE_FILES="${BASE_PATH}/${BASE_FILE_NAME}-"
+  METADATA_FILE="${BASE_FILES}metadata.txt"
+  
+  # Get parameters from metadata file
+  NUM_JOBS=$(grep "NUM_JOBS" $METADATA_FILE | cut -d "=" -f 2)
+  SPLIT_SIZE=$(grep "SPLIT_SIZE" $METADATA_FILE | cut -d "=" -f 2)
+  BASE_FILE_TYPE=$(grep "FILE_TYPE" $METADATA_FILE | cut -d "=" -f 2)
+  BLOCKSIZEBYTES=$(grep "BLOCKSIZEBYTES" $METADATA_FILE | cut -d "=" -f 2)
+  
+  # Debug Info:
+  echo ${BASE_PATH}
+  echo ${BASE_FILE_NAME}
+  echo ${BASE_FILES}\*
+fi
+if [ ! -z $SOURCE ]; then
+  INPUT_FILE=$SOURCE
+  INPUT_FILE_TYPE="$(file -b $SOURCE)"
+fi
+if [ ! -z $DESTINATION ]; then
 OUTPUT_FILE=$DESTINATION
 OUTPUT_FILE_TYPE="$(file -b $DESTINATION)"
-
-echo ${INPUT_PATH}
-echo ${INPUT_FILE_BASENAME}
-echo ${INPUT_FILES}\*
-
-# Get parameters from metadata file
-NUM_JOBS=$(grep "NUM_JOBS" $METADATA_FILE | cut -d "=" -f 2)
-SPLIT_SIZE=$(grep "SPLIT_SIZE" $METADATA_FILE | cut -d "=" -f 2)
-INPUT_FILE_TYPE=$(grep "FILE_TYPE" $METADATA_FILE | cut -d "=" -f 2)
-BLOCKSIZEBYTES=$(grep "BLOCKSIZEBYTES" $METADATA_FILE | cut -d "=" -f 2)
-
-echo ${INPUT_FILE_TYPE}
-echo "DESTINATION: $DESTINATION"
-echo "OUTPUT_FILE: ${OUTPUT_FILE}"
-echo ${OUTPUT_FILE_TYPE}
-if [[ "${INPUT_FILE_TYPE}" == "block special"* ]] && [[ "${OUTPUT_FILE_TYPE}" == "block special"* ]]; then
-  echo "Beginning to check ..."
-  check_restored_image
 fi
-if [[ "${INPUT_FILE_TYPE}" != "block special"* ]] && [[ "${OUTPUT_FILE_TYPE}" != "block special"* ]]; then
-  echo "Beginning to check ..."
-  check_restored_image
+
+
+# Old Debug Info, can be removed
+#echo "BASE_FILE_TYPE: ${BASE_FILE_TYPE}"
+#echo "DESTINATION: $DESTINATION"
+#echo "OUTPUT_FILE: ${OUTPUT_FILE}"
+#echo "OUTPUT_FILE_TYPE: ${OUTPUT_FILE_TYPE}"
+
+if [ ! -z "$SOURCE" ] && [ ! -z "$BASE_PATH" ] && [ -z "$DESTINATION" ]; then
+  echo "Comparing Source $SOURCE with $BASE_PATH ..."
 fi
+
+# Check if only $SOURCE and $BASE_PATH are set
+if [ -n "$SOURCE" ] && [ -n "$BASE_PATH" ] && [ -z "$DESTINATION" ]; then
+  echo "In the loop: Comparing Source $SOURCE with Base Path $BASE_PATH ..."
+
+  # Add code for this iteration here if necessary.
+fi
+
+# Check if only $BASE_PATH and $DESTINATION are set
+if [ -z "$SOURCE" ] && [ -n "$BASE_PATH" ] && [ -n "$DESTINATION" ]; then
+  echo "In the loop: Comparing Base Path $BASE_PATH with Destination $DESTINATION ..."
+
+  if [[ "${BASE_FILE_TYPE}" == "block special"* ]] && [[ "${OUTPUT_FILE_TYPE}" == "block special"* ]]; then
+    echo "Beginning to check ..."
+    check_restored_image
+  fi
+  if [[ "${BASE_FILE_TYPE}" != "block special"* ]] && [[ "${OUTPUT_FILE_TYPE}" != "block special"* ]]; then
+    echo "Beginning to check ..."
+    check_restored_image
+  fi
+
+fi
+
+# Check if only $SOURCE and $DESTINATION are set
+if [ -n "$SOURCE" ] && [ -z "$BASE_PATH" ] && [ -n "$DESTINATION" ]; then
+  echo "In the loop: Comparing Source $SOURCE with Destination $DESTINATION ..."
+
+  # Add code for this iteration here if necessary.
+fi
+
+
 wait
